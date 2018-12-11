1027abf0712894c0ac4ff754f254a144da721736
IMMaurC1 <IMMaurC1@IM10058684.ukt.ad.local>
2018-12-11 14:20:57 +0100

    First Commit
    structer will follow

---
+++ PythonOwnProject/bwHC/db_connections.py
@@ -0,0 +1,17 @@
+'''
+Created on 11.12.2018
+
+@author: IMMaurC1
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
