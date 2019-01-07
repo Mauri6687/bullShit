80863b17f099877c0a013fc15808ac3c387dd756
IMMaurC1 <IMMaurC1@IM10058684.ukt.ad.local>
2019-01-07 14:34:41 +0100

    webServer mit Rest API

--- PythonOwnProject/bwHC/db_connections.py
+++ PythonOwnProject/bwHC/db_connections.py
@@ -4,12 +4,40 @@ Created on 11.12.2018
 @author: IMMaurC1
 '''
 
-class MyClass(object):
+import mysql.connector
+
+class db_connections(object):
     '''
     classdocs
     '''
+    print("Hello world")
 
 
+    mydb = mysql.connector.connect(
+        host="VSWCENTRAXX02",
+        user="sa_zpm_read_all",
+        passwd="pI1UL6x&"
+        )
+
+    print(mydb)
+    
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SHOW DATABASES")
+
+    for x in mycursor:
+      print(x)
+      
+    mycursor.execute("SELECT TOP 10 * FROM current_patient_data")  
+    
+    '''print(mycursor[65])'''
+    
+    
+    for y in mycursor:
+      print(y)  
+
+    
+    
     def __init__(self, params):
         '''
         Constructor
