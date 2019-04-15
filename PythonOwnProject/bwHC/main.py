d4ae0469470d1ced2cadcb055037260ae23860b7
Carlos Maurici <32953871+Mauri6687@users.noreply.github.com>
2019-04-15 15:59:06 +0200

    Add files via upload
    Rest API funktioniert.
    
    Basic Authentifikation includen.

---
+++ PythonOwnProject/bwHC/main.py
@@ -0,0 +1,17 @@
+'''
+Created on 15.04.2019
+
+@author: IMMaurC1
+'''
+
+
+from bwHC.db_connections import db_connections
+
+#database = db_connections.getPatient(5503111)
+
+
+
+
+
+if __name__ == '__main__':
+    pass
--- PythonOwnProject/bwHC/rest_API.py
+++ PythonOwnProject/bwHC/rest_API.py
@@ -4,15 +4,32 @@ Created on 07.01.2019
 @author: IMMaurC1
 
 '''
-#import bwHC.db_connections as db
+import bwHC.db_connections as db
 
 from flask import Flask
+from flask.globals import request
+
 
 app = Flask(__name__)
 
 @app.route('/rest')
-def hello():
+def rest():
     return "Rest API is here"
 
+
+@app.route('/zpm')
+def zpm():
+    return "ZPM is here"
+
+@app.route('/user/<string:username>', methods=['GET'])
+def user(username):
+    return "ZPM is here and you are "+username
+
+@app.route('/patient/mpi=<string:mpi>', methods=['GET'])
+def patient(mpi):
+    output = db.db_connections.getPatient((str(mpi)))        
+    return str(output)
+
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=80)
+    app.run(host='10.231.37.198', port=80)
